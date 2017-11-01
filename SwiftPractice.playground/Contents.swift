a802961b6f97d0a972fd9a86af9bafbb5808d2b2
robzdoom <jorgecruz.rd@gmail.com>
2017-11-01 16:06:58 -0700

    class/func

--- SwiftPractice.playground/Contents.swift
+++ SwiftPractice.playground/Contents.swift
@@ -3,3 +3,26 @@
 import UIKit
 
 var str = "Hello, playground"
+
+class Person {
+    var firstName: String //Make sure they are not equal.
+    var lastName: String
+    
+    init (firstName: String, lastName: String){
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+    func say(phrase: String){
+        print("\(firstName) \(lastName) says, '\(phrase)'")
+        
+    }
+    class func say(phrase: String) {
+        print ("... and the people all say '\(phrase)'")
+    }
+}
+
+let p1 = Person(firstName: "Guile", lastName: "Benize")
+p1.say(phrase: "Lets Dooooooo it")
+
+Person.say(phrase: "Lets get tacos")
