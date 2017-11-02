f998c51d69dd200b20f2bb38f5b2a4aa19fdae33
robzdoom <jorgecruz.rd@gmail.com>
2017-11-02 10:35:32 -0700

    added page

--- SwiftBasics.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ SwiftBasics.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -22,4 +22,16 @@ say(phrase: "Yeallow", times:2)
 
 let names = ["Al", "Ann", "Alex", "Alice", "Audrey" ]
 
+var tr  = "Hello M'lad"
+
+tr = "Yonkers"
+
+print (tr)
+
+// Working on Constants
+
+let language = "Swift"
+// Can't change is anymore. Let locks in the value
+
+var 🤡 = "Clownie TootErson"
 
---
+++ SwiftBasics.playground/Pages/types.xcplaygroundpage/Contents.swift
@@ -0,0 +1,12 @@
+//: [Previous](@previous)
+
+import Foundation
+
+var str = "Hello, playground"
+
+//: [Next](@next)
+let country = "United States "
+let state = "North Carolina"
+let city = "Charlotte"
+
+let address = country + state + city
