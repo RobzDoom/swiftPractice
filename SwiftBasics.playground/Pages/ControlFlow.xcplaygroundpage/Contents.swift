96f436eeaf07a9952fb7ce349a4a05b80e94bfff
robzdoom <jorgecruz.rd@gmail.com>
2017-11-08 14:20:09 -0800

    ControlFlow

---
+++ SwiftBasics.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,57 @@
+//: [Previous](@previous)
+
+import Foundation
+
+// Arrays
+
+var todo: [String] = ["Finish Collection Course", "Buy groceries", "Respond to Email"]
+var numbers: [Int] = [1,2,3]
+
+//Add item to arrat using Append
+todo.append("Pick up dry cleaning")
+
+//Concatenating two arrays
+
+[1,2,3] + [4]
+
+// Either one of these concatinations will work
+todo = todo + ["Order book online"]
+todo += ["Order book online"]
+
+//Immutable Arrays
+
+let secondTaskList = ["Mow the lawn"]
+
+// secondTaskList has let which means it cannot be changed.
+//secondTaskList.append('Pay Bills') Error!
+//secondTaskList += ["Some Value"]
+
+let firstTask = todo[0]
+let thirdTask = todo[2]
+
+//modifying Existing Values in and Array
+//(Mutating an array)
+
+todo[4] = "Brush teeth"
+todo[0] = "Watch some videos about stuff"
+
+//Inserting using indexes
+todo.insert("Watch TV", at: 2)
+
+//How to remove items from arrays
+todo.remove(at: 2)
+
+todo.count
+var a = todo.count-1
+todo[a]
+
+//Dictionaries // This is new to me.
+let airport:[String: String] = [
+    "LGA": "La Guardia",
+    "LHR": "HearthRow",
+    "CDG": "Charles De Gualle",
+    "HKG": "Hong Kong International"
+    "DXB": "Dubai International"
+]
+
+let t = 
